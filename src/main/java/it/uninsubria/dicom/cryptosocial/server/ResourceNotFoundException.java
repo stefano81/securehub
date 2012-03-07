8b4d6166dc90d6944daeaa030ee96f579e051dbd
Stefano Braghin <stefano.braghin@gmail.com>
2012-03-07 17:15:28 +0800

    I forgot these classes

---
+++ src/main/java/it/uninsubria/dicom/cryptosocial/server/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package it.uninsubria.dicom.cryptosocial.server;
+
+public class ResourceNotFoundException extends Exception {
+	private static final long serialVersionUID = -284110147077738987L;
+}
---
+++ src/test/java/it/uninsubria/dicom/cryptosocial/shared/DBRASetupTest.java
@@ -0,0 +1,25 @@
+package it.uninsubria.dicom.cryptosocial.shared;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBRASetupTest {
+	DBRASetup setup;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
